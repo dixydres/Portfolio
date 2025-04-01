58ebe5ea1efb3280307981879b5e73b6303019e1
dixydres <jeanjose.muller@etu.unilim.fr>
2025-04-01 11:23:12 +0200

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://dixydres.github.io',
+  base: 'Portfolio',
   integrations: [mdx(), sitemap()],
 
   vite: {
