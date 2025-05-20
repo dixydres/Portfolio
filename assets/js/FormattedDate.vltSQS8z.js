c54aa259b366e52134b4a6c68840e33e6d42fc8e
dixydres <jeanjose.muller@etu.unilim.fr>
2025-05-20 23:22:32 +0200

    Updates

---
+++ assets/js/FormattedDate.vltSQS8z.js
@@ -0,0 +1,68 @@
+import { g as createCollectionToGlobResultMap, h as createGetCollection, d as createAstro, c as createComponent, m as maybeRenderHead, b as addAttribute, a as renderTemplate } from './astro.CPSAduzO.js';
+import 'kleur/colors';
+import 'clsx';
+
+// astro-head-inject
+
+const contentDir = '/src/content/';
+
+const contentEntryGlob = "";
+const contentCollectionToEntryMap = createCollectionToGlobResultMap({
+	globResult: contentEntryGlob,
+	contentDir,
+});
+
+const dataEntryGlob = "";
+const dataCollectionToEntryMap = createCollectionToGlobResultMap({
+	globResult: dataEntryGlob,
+	contentDir,
+});
+createCollectionToGlobResultMap({
+	globResult: { ...contentEntryGlob, ...dataEntryGlob },
+	contentDir,
+});
+
+let lookupMap = {};
+lookupMap = {};
+
+new Set(Object.keys(lookupMap));
+
+function createGlobLookup(glob) {
+	return async (collection, lookupId) => {
+		const filePath = lookupMap[collection]?.entries[lookupId];
+
+		if (!filePath) return undefined;
+		return glob[collection][filePath];
+	};
+}
+
+const renderEntryGlob = "";
+const collectionToRenderEntryMap = createCollectionToGlobResultMap({
+	globResult: renderEntryGlob,
+	contentDir,
+});
+
+const cacheEntriesByCollection = new Map();
+const getCollection = createGetCollection({
+	contentCollectionToEntryMap,
+	dataCollectionToEntryMap,
+	getRenderEntryImport: createGlobLookup(collectionToRenderEntryMap),
+	cacheEntriesByCollection,
+});
+
+const $$Astro = createAstro("https://dixydres.github.io");
+const $$FormattedDate = createComponent(($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
+  Astro2.self = $$FormattedDate;
+  const { date } = Astro2.props;
+  function formatDate(date2) {
+    return new Date(date2).toLocaleDateString("fr-FR", {
+      year: "numeric",
+      month: "long",
+      day: "numeric"
+    });
+  }
+  return renderTemplate`${maybeRenderHead()}<time${addAttribute(date.toISOString(), "datetime")}> ${formatDate(date)} </time>`;
+}, "C:/Users/jeanj/OneDrive/Bureau/Perso/PortfolioAstro/src/components/FormattedDate.astro", void 0);
+
+export { $$FormattedDate as $, getCollection as g };
